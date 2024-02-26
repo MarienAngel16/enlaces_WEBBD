2446cba840c510fc085545b3f145abe4b66041c8
MarienAngel16 <emary.presc16@gmail.com>
2024-02-25 22:45:21 -0600

    cambios en el muestra enlaces

--- funciones/muestra_enlaces.php
+++ funciones/muestra_enlaces.php
@@ -4,39 +4,62 @@ include_once 'base.php';
 $consultaContenidos = "SELECT clave, nombre, total FROM contenidos";
 $resultadoContenidos = mysqli_query($conexion, $consultaContenidos);
 
-if (mysqli_num_rows($resultadoContenidos) > 0) {
-    echo '<div class="btn-group-vertical text-center">';
-    while ($filaContenido = mysqli_fetch_assoc($resultadoContenidos)) {
-        $claveContenido = $filaContenido['clave'];
-        $nombreContenido = $filaContenido['nombre'];
-        $total = $filaContenido['total'];
 
-        echo '<a href="funciones/muestra_enlaces.php?clave_en=' . $claveContenido . '" class="btn btn-primary bg-gradient rounded" style="margin: 1px; color: white; font-size: 120%;">' . $claveContenido . ' - ' . $nombreContenido . ' - ' .$total. '</a><br>';
-    }
-    echo '</div>';
-} else {
-    echo "No se encontraron contenidos.";
+if (mysqli_num_rows($resultadoContenidos) > 0) {
+
+    echo '<div class="text-center">';
+echo '<div style="display: flex; flex-wrap: wrap;">';
+
+while ($filaContenido = mysqli_fetch_assoc($resultadoContenidos)) {
+    $claveContenido = $filaContenido['clave'];
+    $nombreContenido = $filaContenido['nombre'];
+    $total = $filaContenido['total'];
+
+    echo '
+    
+        <div class="card" style="width: 15rem;">      
+            <div class="card-body">
+                <h5 class="card-title">' . $claveContenido . ' - ' . $nombreContenido . ' - ' . $total . '</h5>
+                <div class=".text-light muestra_enlaces"> ';
+
+
+                if ($claveContenido) {                    
+                    
+                    $consultaEnlaces = "SELECT nombre_en, enlace FROM enlaces WHERE clave='$claveContenido'";    
+                
+                    $resultadoEnlaces = mysqli_query($conexion, $consultaEnlaces);
+                
+                    if (mysqli_num_rows($resultadoEnlaces) > 0) {
+                        echo '<div class="btn-group-vertical text-center">';
+                        while ($filaEnlace = mysqli_fetch_assoc($resultadoEnlaces)) {
+                            $nombreEnlace = $filaEnlace['nombre_en'];
+                            $linkEnlace = $filaEnlace['enlace'];
+                
+                            echo '<a href="' . $linkEnlace . '" style="margin: 1px;">' . $nombreEnlace . '</a>
+                                  <img src="img/enlace_icono.png" alt=""> 
+                            ';
+                            
+                        }
+                        echo '</div>';
+                    } else {
+                        echo "No se encontraron enlaces.";
+                    }
+                }
+
+
+     echo '
+                </div>
+            </div>
+        </div>    
+    ';
 }
 
-if (isset($_GET["clave_en"])) {
-    $claveEnlace = mysqli_real_escape_string($conexion, $_GET["clave_en"]);
+echo '</div>';
+echo '</div>';
+
     
-    $consultaEnlaces = "SELECT nombre_en, enlace FROM enlaces WHERE clave='$claveEnlace'";    
-
-    $resultadoEnlaces = mysqli_query($conexion, $consultaEnlaces);
-
-    if (mysqli_num_rows($resultadoEnlaces) > 0) {
-        echo '<div class="btn-group-vertical text-center">';
-        while ($filaEnlace = mysqli_fetch_assoc($resultadoEnlaces)) {
-            $nombreEnlace = $filaEnlace['nombre_en'];
-            $linkEnlace = $filaEnlace['enlace'];
-
-            echo '<a href="' . $linkEnlace . '" class="btn btn-primary bg-gradient rounded" style="margin: 1px; color: white; font-size: 120%;">' . $nombreEnlace . '</a><br>';
-        }
-        echo '</div>';
-    } else {
-        echo "No se encontraron enlaces.";
-    }
+} else {
+    echo "No se encontraron contenidos.";
 }
 
 mysqli_close($conexion);
